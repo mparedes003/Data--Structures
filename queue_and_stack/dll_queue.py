746a0a76bce1a3eda883b00c10231061f2265cbf
mparedes003 <onnabugeishadev@gmail.com>
2019-10-14 12:10:55 -0700

    Worked on dll_queue.py with 2 out of 4 tests passing.

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,35 @@
+from doubly_linked_list import DoublyLinkedList
 import sys
 sys.path.append('../doubly_linked_list')
-from doubly_linked_list import DoublyLinkedList
 
 
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        # enqueue means we are adding to the back/tail of the DLL
+        # we need to increment/increase storage by 1
+        #self.storage.length += 1
+        # use add_to_tail() to add the value in a new node
+        # to the back/tail of the DLL
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        # dequeue means we are subtracting/removing from the front/head of the DLL
+        # we need to decrement/decrease storage by 1
+        #self.storage.length -= 1
+        # if there is no node in storage
+        if self.storage.head == None:
+            # return None
+            return None
+
+        else:
+            # use remove_from_head() to delete the first node/the head
+            self.storage.remove_from_head()
 
     def len(self):
-        pass
+        # return the size of the queue
+        return self.size
